da3e952cecb8227426f96122bc42c244f135366c
Samy Mahmod <symq9485@gmail.com>
2017-01-09 00:39:59 -0400

    first commit

---
+++ Promedio_Notas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Promedio_Notas
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double Nota = 0.00D;
+			int Aux = -1;
+			int i = 0;
+			char P = 'y';
+
+			while (Aux != 0) 
+			{
+				Console.WriteLine ("Ingrese la nota: ");
+				Aux = Convert.ToInt16(Console.ReadLine());
+				Nota = Nota + Aux;
+				i= ++i;
+
+
+				if (Aux == 0)
+				{
+					Console.Write ("Desea incluir la nota 0...? (y/n): ");
+					P = Convert.ToChar(Console.ReadLine());
+					Aux = Aux++;
+
+					if (P == 'n')
+						i = --i;
+				}
+			}
+			Nota = Nota / i;
+			Console.WriteLine ("\nLa nota promedio es: "+Nota);
+			Console.WriteLine ("\nPulse una tecla para salir.");
+			Console.ReadKey();
+		}
+	}
+}
